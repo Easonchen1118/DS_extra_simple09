6a717d7251d014027cb8ba485ee68a7c1cd9b0d3
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-20 21:23:24 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//計算正整數被3整除之數值之總和
+int main(){
+    int num;
+    int ans = 0;
+    scanf("%d", &num);
+    for (int i=0; i<=num; i++) {
+        if (i % 3) {
+            continue;
+        }
+        else {
+            ans += i;
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
